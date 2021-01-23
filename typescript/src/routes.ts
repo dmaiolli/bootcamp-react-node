35760d75c289cb90d8d7210f728499655fcb2b3e
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2021-01-22 23:42:10 -0300

    typing object and array

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,18 @@
 import { Request, Response } from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
+    const user = createUser({
+        name: 'Denys',
+        email: 'denys@maiolli',
+        password: '1515',
+				techs: [
+					'Node.js',
+					'React Native',
+					'React JS',
+				    { title: 'Javascript', experience: 100 },
+				]
+    });
+
     return response.json({ message: 'Hello World'})
 }
