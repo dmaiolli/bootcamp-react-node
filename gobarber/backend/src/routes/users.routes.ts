29d42312f3ffde1ed17a6393202ea54138709866
Denys Lonkovski Maiolli <denysmaiolli@hotmail.com>
2021-02-03 21:52:00 -0300

    create: user register

---
+++ gobarber/backend/src/routes/users.routes.ts
@@ -0,0 +1,28 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import { Router } from 'express';
+
+import CreateUserService from '../services/CreateUserService';
+
+const usersRouter = Router();
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      // await pois nosso método execute é assíncrono
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default usersRouter;
